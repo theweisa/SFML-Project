c845eedfa01fcec5b59a66d64a0c806b4f3d84dc
theweisa <weiandrew2012@gmail.com>
2023-11-27 01:25:21 -0800

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "pong/pong.h"
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+    Pong pong;
+    pong.Run();
+    return 0;
 }
---
+++ src/pong/pong.h
@@ -0,0 +1,10 @@
+#ifndef PONG_H
+#define PONG_H
+
+#include "../game/game.h"
+
+class Pong : public Game {
+
+};
+
+#endif
